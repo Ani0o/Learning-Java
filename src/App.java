96a169c788166f52fd14f207e20dfdd660201f07
Ani <205335742+Ani0o@users.noreply.github.com>
2025-11-22 01:29:26 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Comment
+        /* Same as C */
+        
+        System.out.print("Enter current year: ");
+        int year = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Year is " + year);
+        System.out.print("Hello, " + name);
+
+        scanner.close();
+    }
+}
